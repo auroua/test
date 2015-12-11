5ede3e255edac5315a61ae8da217043d9e110ec2
aurora <auroua@yeah.net>
2015-12-11 12:50:21 +0800

    implements dsift and knn

--- img_proc/img_tools.py
+++ img_proc/img_tools.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 import numpy as np
 from matplotlib import pylab
-
+import matplotlib.pyplot as plt
 
 def get_imlist(path):
     '''return the absolute path of the image files in the path folder'''
@@ -34,6 +34,36 @@ def compute_average(imlist):
     averageim /= len(imlist)
 
     return np.array(averageim, 'uint8')
+
+
+def plot_2D_boundary(plot_range,points,decisionfcn,labels,values=[0]):
+    """    Plot_range is (xmin,xmax,ymin,ymax), points is a list
+        of class points, decisionfcn is a funtion to evaluate,
+        labels is a list of labels that decisionfcn returns for each class,
+        values is a list of decision contours to show. """
+
+    clist = ['b','r','g','k','m','y'] # colors for the classes
+
+    # evaluate on a grid and plot contour of decision function
+    x = np.arange(plot_range[0],plot_range[1],.1)
+    y = np.arange(plot_range[2],plot_range[3],.1)
+    xx,yy = np.meshgrid(x,y)
+    xxx,yyy = xx.flatten(),yy.flatten() # lists of x,y in grid
+    zz = np.array(decisionfcn(xxx,yyy))
+    zz = zz.reshape(xx.shape)
+    # plot contour(s) at values
+    plt.contour(xx,yy,zz,values)
+
+    # for each class, plot the points with '*' for correct, 'o' for incorrect
+    for i in range(len(points)):
+        d = decisionfcn(points[i][:,0],points[i][:,1])
+        correct_ndx = labels[i]==d
+        incorrect_ndx = labels[i]!=d
+        plt.plot(points[i][correct_ndx,0],points[i][correct_ndx,1],'*',color=clist[i])
+        plt.plot(points[i][incorrect_ndx,0],points[i][incorrect_ndx,1],'o',color=clist[i])
+
+    plt.axis('equal')
+
 
 
 if __name__ == '__main__':
